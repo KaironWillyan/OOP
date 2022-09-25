e535f9e172d425b51dae7b6465acbd950d5e40cd
Kairon Willyan <willyankairon1@gmail.com>
2022-09-25 20:43:17 -0300

    commit

--- ATV_02/QuestsTSC/questao_7.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let nome = "Ely";
-let tempo = 120.56;
-let nome_linguagem = "TypeScript";
-console.log(`${nome}
-My payment time is ${tempo}
-and
-my preffered language is ${nome_linguagem}`);
